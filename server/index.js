ac68e8660746292af00ad60a9a74cce2fdb433c8
Anirudh A V <anirudh.av02@gmail.com>
2023-03-14 12:48:03 +0530

    feat: Basic express app initialised with cors

--- server/index.js
+++ server/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => {
+    console.log(`API listening on port ${port}!`);
+});
