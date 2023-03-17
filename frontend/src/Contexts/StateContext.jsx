82661d57a6a9b3325cf3ef800f4a5b7aa5af4c81
Anirudh A V <anirudh.av02@gmail.com>
2023-03-17 18:00:21 +0530

    fix: context API fixed

--- frontend/src/Contexts/StateContext.jsx
+++ frontend/src/Contexts/StateContext.jsx
@@ -3,10 +3,16 @@ import React, { createContext, useContext, useState } from 'react';
 const StateContext = createContext();
 
 export const ContextProvider = ({ children }) => {
-    
+    const [login, setLogin] = useState(false)
+    const [selectedFile, setSelectedFile] = useState(null);
+
     return (
         <StateContext.Provider
             value={{
+                login,
+                setLogin,
+                selectedFile,
+                setSelectedFile
             }}
         >
             {children}
