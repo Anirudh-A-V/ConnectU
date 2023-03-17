66a4f3ac9c49bdcdf7b6298c85643e5553133941
Anirudh A V <anirudh.av02@gmail.com>
2023-03-17 18:00:36 +0530

    feat: Register page finished

---
+++ frontend/src/Pages/Register.jsx
@@ -0,0 +1,193 @@
+import './styles.css'
+import Login from './Login'
+import Signup from './Signup'
+import { useState } from 'react'
+import { useStateContext } from '../Contexts/StateContext'
+
+const Register = () => {
+    // const { login, selectedFile, setLogin, setSelectedFile } = useStateContext;
+    const [login, setLogin] = useState(false);
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileInputChange = (event) => {
+        setSelectedFile(event.target.files[0]);
+    };
+
+    return (
+        <div className="App flex">
+            <div className="bg-image w-full h-screen">
+            </div>
+            {login ? (
+                <div className="login-box flex flex-col items-center justify-center w-1/2 min-h-screen border">
+                    <div className="m-8">
+                        <h1 className="font-medium text-4xl text-[#151414]">
+                            Hey there{" "}
+                            <span role="img" aria-label="">
+                                👋
+                            </span>
+                        </h1>
+                        <p className="font-normal text-xs text-[#757373] m-2">
+                            Enter the information you entered while registering
+                        </p>
+                    </div>
+                    <div class="flex justify-center">
+                        <div>
+                            <div class="relative mb-6 xl:w-96">
+                                <input
+                                    type="email"
+                                    class="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-600 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none  [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
+                                    id="floatingInput"
+                                    placeholder="name@example.com"
+                                />
+                                <label
+                                    for="floatingInput"
+                                    class="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-600 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none"
+                                >
+                                    Email address
+                                </label>
+                            </div>
+                            <div class="relative mb-3 xl:w-96">
+                                <input
+                                    type="password"
+                                    class="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-600 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none  [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
+                                    id="floatingPassword"
+                                    placeholder="Password"
+                                />
+                                <label
+                                    for="floatingPassword"
+                                    class="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-600 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none"
+                                >
+                                    Password
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="buttons flex w-4/5 items-center justify-between mt-3">
+                        <div className="flex cursor-pointer">
+                            <input type="checkbox" className="mr-1 cursor-pointer" />
+                            <label className="text-xs font-semibold text-[#272727] cursor-pointer">
+                                Remember me
+                            </label>
+                        </div>
+                        <p className="text-xs font-light text-[#1487df] cursor-pointer">
+                            Forgot password?
+                        </p>
+                    </div>
+                    <div className="font-medium text-md p-4 mt-6 w-4/5 h-10 bg-transparent border text-[#222221] hover:bg-[#040404] hover:text-[#f9f9f5] border-black transition duration-150 ease-in-out rounded-md flex justify-center items-center">
+                        Login
+                    </div>
+                    <div className="flex mt-4">
+                        <p className="text-sm font-light flex mr-1">
+                            Don't have an account?
+                        </p>{" "}
+                        <p className="text-sm font-light text-[#1486de] hover:text-[#096ab5] cursor-pointer" onClick={() => setLogin(false)}>
+                            Sign Up
+                        </p>
+                    </div>
+                </div>
+            ) : (
+                <div className="login-box flex flex-col items-center justify-center w-1/2 min-h-screen border">
+                    <div className="m-8 flex flex-col items-center justify-center">
+                        <h1 className="font-medium text-4xl text-[#151414]">
+                            Get Started{" "}
+                            <span role="img" aria-label="">
+                                👍
+                            </span>
+                        </h1>
+                        <p className="font-normal flex text-xs text-[#757373] m-2">
+                            Create an Account now
+                        </p>
+                    </div>
+                    <div class="flex justify-center">
+                        <div>
+                            <div className="flex justify-between">
+                                <div class="relative mb-6 xl:w-44">
+                                    <input
+                                        type="text"
+                                        class="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-600 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none  [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
+                                        id="floatingInput"
+                                        placeholder="name@example.com"
+                                    />
+                                    <label
+                                        for="floatingInput"
+                                        class="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-600 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none"
+                                    >
+                                        First Name
+                                    </label>
+                                </div>
+                                <div class="relative mb-6 xl:w-44">
+                                    <input
+                                        type="text"
+                                        class="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-600 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none  [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
+                                        id="floatingInput"
+                                        placeholder="name@example.com"
+                                    />
+                                    <label
+                                        for="floatingInput"
+                                        class="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-600 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none"
+                                    >
+                                        Last Name
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="relative mb-6 xl:w-96">
+                                <input
+                                    type="email"
+                                    class="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-600 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none  [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
+                                    id="floatingInput"
+                                    placeholder="name@example.com"
+                                />
+                                <label
+                                    for="floatingInput"
+                                    class="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-600 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none"
+                                >
+                                    Email address
+                                </label>
+                            </div>
+                            <div class="relative mb-3 xl:w-96">
+                                <input
+                                    type="password"
+                                    class="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-600 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none  [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
+                                    id="floatingPassword"
+                                    placeholder="Password"
+                                />
+                                <label
+                                    for="floatingPassword"
+                                    class="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-600 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none"
+                                >
+                                    Password
+                                </label>
+                            </div>
+                            <div className="relative mb-3 xl:w-96">
+                                <div className="w-full border border-gray-300 rounded-lg flex items-center">
+                                    <label class="flex items-center justify-center text-sm font-medium text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-100 focus:outline-none w-40 h-10" for="file_input">Upload file</label>
+                                    <input class=" w-full text-sm hidden text-gray-800 border hover:bg-gray-600 border-gray-300 rounded-lg cursor-pointer bg-gray-500 focus:outline-none" aria-describedby="file_input_help" id="file_input" type="file" onChange={handleFileInputChange} />
+                                    <div className="w-full flex items-center justify-center">
+                                        {selectedFile && <p className="text-sm text-gray-700">{selectedFile.name}</p>}
+                                    </div>
+                                </div>
+
+                                <p class="mt-1 text-sm text-gray-500" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="font-medium text-md p-4 mt-6 w-4/5 h-10 bg-transparent border text-[#222221] hover:bg-[#040404] hover:text-[#f9f9f5] border-black transition duration-150 ease-in-out rounded-md flex justify-center items-center">
+                        Sign Up
+                    </div>
+                    <div className="flex mt-4">
+                        <p className="text-sm font-light flex mr-1">
+                            Have an account?
+                        </p>{" "}
+                        <div className="text-sm font-light text-[#1486de] hover:text-[#096ab5] cursor-pointer" onClick={() => setLogin(true)}>
+                            Login
+                        </div>
+                    </div>
+                </div>
+            )}
+            {/* {login ? (<Login setLogin={setLogin} />) : (<Signup setLogin={setLogin} />)} */}
+        </div>
+    )
+}
+
+export default Register
