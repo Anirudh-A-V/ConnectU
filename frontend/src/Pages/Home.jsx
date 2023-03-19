f30dc96945ea2ccc73c1d1b461bb5a50e5c77eb8
Anirudh A V <anirudh.av02@gmail.com>
2023-03-19 18:04:51 +0530

    feat: Home page now displays all users
    This commit adds a new component to the Home page that displays all users in the database.
    And it is made responsive for mobile devices.

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -5,13 +5,13 @@ import { useEffect } from "react"
 import { useStateContext } from "../Contexts/StateContext"
 const Home = () => {
 
-    const { token, users, setUsers } = useStateContext()
+    const { users, setUsers } = useStateContext()
     const navigate = useNavigate()
 
     const Token = localStorage.getItem("token")
 
     const fetchUsers = async () => {
-        const response = fetch("http://localhost:3000/users", {
+        const response = fetch(`${import.meta.env.VITE_API_URI}/users`, {
             method: "GET",
             headers: {
                 "Authorization": `Bearer ${Token}`
@@ -20,10 +20,10 @@ const Home = () => {
             .then(res => res.json())
             .then(data => {
                 console.log(data)
-                setUsers(data.users)
-                console.log(data.users)
+                const Users = data.users.filter((user) => {return user._id != localStorage.getItem("id").replace(/"/g, "")})
+                setUsers(Users)
+                // console.log(Users)
             })
-
     }
 
     useEffect(() => {
@@ -37,13 +37,6 @@ const Home = () => {
         <div className="flex justify-center items-center h-full flex-wrap">
             <Navbar />
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-5">
-                {/* <Cards slug="/" />
-                <Cards />
-                <Cards />
-                <Cards />
-                <Cards />
-                <Cards />
-                <Cards /> */}
                 {users.map((user) => {
                     return (
                         <Cards slug={user._id} image={user.image} name={user.name} bio={user.bio}/>
