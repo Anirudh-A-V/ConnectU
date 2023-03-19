8eae81f2bb3b98115b993bce1e37ab0cd6cc512f
Anirudh A V <anirudh.av02@gmail.com>
2023-03-19 17:59:36 +0530

    reafctor: Conditional rendering  implemented
    If there are no mutuals, the mutuals component will not render
    It will show a message saying that there are no mutuals

--- frontend/src/Components/Mutuals.jsx
+++ frontend/src/Components/Mutuals.jsx
@@ -22,29 +22,35 @@ const Mutuals = (props) => {
                 <div className="w-[80%] h-[1px] bg-gray-300 "></div>
                 <p className="text-xl font-normal text-gray-800 mt-2 mb-3">Mutuals</p>
             </div>
-            <div className=" flex items-center justify-between">
-                <button className="h-10 w-10 bg-gray-50 text-gray-500 flex items-center justify-center" onClick={leftScroll}>
-                    <BiChevronLeft className="h-8 w-8" />
-                </button>
-                <div className='flex w-full overflow-x-scroll' id='mutuals'>
-                    {props.mutualFriends.map((friend) =>
-                        <Link to={`/in/${friend._id}`}>
-                            <div class="min-w-[240px] w-52 bg-white rounded-xl h-60 flex flex-col justify-center mx-2 items-center border cursor-pointer">
-                                <div class="mb-8">
-                                    <img class="object-center object-cover rounded-full h-24 w-24" src={friend.image} alt="photo" />
+            {props.mutualFriends.length > 0 ?
+                <div className=" flex items-center justify-between">
+                    <button className="h-10 w-10 bg-gray-50 text-gray-500 flex items-center justify-center" onClick={leftScroll}>
+                        <BiChevronLeft className="h-8 w-8" />
+                    </button>
+                    <div className='flex w-full overflow-x-scroll' id='mutuals'>
+                        {props.mutualFriends.map((friend) =>
+                            <Link to={`/in/${friend._id}`}>
+                                <div class="min-w-[240px] w-52 bg-white rounded-xl h-60 flex flex-col justify-center mx-2 items-center border cursor-pointer">
+                                    <div class="mb-8">
+                                        <img class="object-center object-cover rounded-full h-24 w-24" src={friend.image} alt="photo" />
+                                    </div>
+                                    <div class="text-center">
+                                        <p class="text-xl text-gray-700 font-bold mb-2">{friend.name.first} {friend.name.last}</p>
+                                        <p class="text-base text-gray-400 font-normal">{friend.bio}</p>
+                                    </div>
                                 </div>
-                                <div class="text-center">
-                                    <p class="text-xl text-gray-700 font-bold mb-2">{friend.name.first} {friend.name.last}</p>
-                                    <p class="text-base text-gray-400 font-normal">{friend.bio}</p>
-                                </div>
-                            </div>
-                        </Link>
-                    )}
+                            </Link>
+                        )}
+                    </div>
+                    <button onClick={rightScroll} className="h-10 w-10 bg-gray-50 text-gray-500 flex items-center justify-center">
+                        <BiChevronRight className="h-8 w-8" />
+                    </button>
                 </div>
-                <button onClick={rightScroll} className="h-10 w-10 bg-gray-50 text-gray-500 flex items-center justify-center">
-                    <BiChevronRight className="h-8 w-8" />
-                </button>
-            </div>
+                :
+                <div className="flex flex-col justify-center items-center">
+                    <p className="text-lg font-normal text-gray-600 mb-3">No mutuals</p>
+                </div>
+            }
         </div>
     );
 };
