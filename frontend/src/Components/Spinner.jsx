03e82e6006230513d89455fa3380f788bfbd55da
Anirudh A V <anirudh.av02@gmail.com>
2023-03-19 18:02:04 +0530

    feat: Spinner component made reusable
    Spinner component can now be used in any component by importing it and passing the props for the size of the viewbox.

--- frontend/src/Components/Spinner.jsx
+++ frontend/src/Components/Spinner.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Spinner = () => {
+const Spinner = (props) => {
     return (
-        <div className="flex justify-center items-center w-full h-screen">
+        <div className={props.screen ? "flex justify-center items-center w-full h-screen" : "flex justify-center items-center p-4 w-full h-full"}>
             <div
-                class="inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                className="inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
                 role="status">
                 <span
-                    class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+                    className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
                 >Loading...</span>
             </div>
 
