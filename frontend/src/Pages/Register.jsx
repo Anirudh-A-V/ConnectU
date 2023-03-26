93244566a3d0e121ebe629749d4012b3cc443a94
Anirudh A V <anirudh.av02@gmail.com>
2023-03-26 09:11:55 +0530

    feat: App name added

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -2,16 +2,30 @@ import './styles.css'
 import Login from './Login'
 import Signup from './Signup'
 import { useStateContext } from '../Contexts/StateContext'
+import { SiQuantconnect } from 'react-icons/si'
+import { useEffect, useState } from 'react'
 
 const Register = () => {
 
     const { login } = useStateContext();
 
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+    useEffect(() => {
+        setWindowWidth(windowWidth);
+    }, [window.innerWidth])
+
     return (
         <div className="App flex">
-            {window.innerWidth > 768 ? (
+            {windowWidth > 768 ? (
                 <>
                     <div className="bg-image w-full h-screen">
+                        <div className="flex justify-center mt-56 w-full h-full">
+                            <div className="flex items-center justify-center w-fit h-fit blur-s">
+                                <SiQuantconnect className="text-9xl z-10 text-[#e2e0e0] mr-4 " />
+                                <h1 className="text-8xl font-bold text-[#e2e0e0]">ConnectU</h1>
+                            </div>
+                        </div>
                     </div>
                     {login ? (<Login />) : (<Signup />)}
                 </>
