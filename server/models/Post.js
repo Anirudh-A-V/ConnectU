d6c8964fc9857a058989d51de95a2491b8c2e95c
Anirudh A V <anirudh.av02@gmail.com>
2023-03-28 23:22:36 +0530

    feat: Post Model created

---
+++ server/models/Post.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const { ObjectId } = Schema.Types;
+
+
+
+const postSchema = mongoose.Schema({
+    user: {
+        type: ObjectId,
+        ref: 'User',
+    },
+    images: {
+        type: [String],
+        required: false,
+    },
+    caption: {
+        type: String,
+        required: true,
+    },
+    likes: {
+        type: [ObjectId],
+        required: false,
+    },
+    noOfLikes: {
+        type: Number,
+        default: 0,
+    },
+    comments: {
+        type: [{
+            user: {
+                type: ObjectId,
+                ref: 'User',
+            },
+            comment: {
+                type: String,
+                required: true,
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now,
+            },
+        }],
+        required: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('Post', postSchema);
