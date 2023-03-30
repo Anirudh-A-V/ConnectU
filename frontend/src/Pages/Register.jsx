f9545362dd611f48fca915a32472526fa67bdbd4
Anirudh A V <anirudh.av02@gmail.com>
2023-03-30 08:51:35 +0530

    temp fix: Register page styles

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -20,7 +20,7 @@ const Register = () => {
             {windowWidth > 768 ? (
                 <>
                     <div className="bg-image w-full h-screen">
-                        <div className="flex justify-center mt-56 w-full h-full">
+                        <div className="flex justify-center mt-56 w-full">
                             <div className="flex items-center justify-center w-fit h-fit blur-s">
                                 <SiQuantconnect className="text-9xl z-10 text-[#e2e0e0] mr-4 " />
                                 <h1 className="text-8xl font-bold text-[#e2e0e0]">ConnectU</h1>
